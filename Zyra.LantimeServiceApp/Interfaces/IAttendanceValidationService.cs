ac926386a388b7695075bf5088454b6698192890
Sandeep <sandeepkumarkrprabhu@gmail.com>
2026-09-25 13:35:43 +0530

    Define attendance validation results

--- Zyra.LantimeServiceApp/Interfaces/IAttendanceValidationService.cs
+++ Zyra.LantimeServiceApp/Interfaces/IAttendanceValidationService.cs
@@ -1,3 +1,4 @@
+using Zyra.LantimeServiceApp.Models;
 using ZyraHangfireModels.Models;
 using ZyraHangfireModels.ServiceModels;
 
@@ -5,12 +6,12 @@ namespace Zyra.LantimeServiceApp.Interfaces
 {
     public interface IAttendanceValidationService
     {
-        Task<bool> ValidateCheckInAsync(
+        Task<AttendanceValidationResult> ValidateCheckInAsync(
             EmployeeMapperDto employee,
             EmployeeAttendancePolicy policy,
             AttendanceDto attendance);
 
-        bool ValidateCheckOut(
+        AttendanceValidationResult ValidateCheckOut(
             EmployeeMapperDto employee,
             EmployeeAttendancePolicy policy,
             AttendanceDto attendance);
