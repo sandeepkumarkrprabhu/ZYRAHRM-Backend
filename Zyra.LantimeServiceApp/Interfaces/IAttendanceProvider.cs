96629a1f0720d14b2e66b9e19671fe928ed8497f
Sandeep <sandeepkumarkrprabhu@gmail.com>
2026-09-25 13:43:00 +0530

    refactor: extract IAttendanceProvider responsibilities

--- Zyra.LantimeServiceApp/Interfaces/IAttendanceProvider.cs
+++ Zyra.LantimeServiceApp/Interfaces/IAttendanceProvider.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Zyra.LantimeServiceApp.Models;
 using ZyraHangfireModels.Models;
 
@@ -10,19 +5,9 @@ namespace Zyra.LantimeServiceApp.Interfaces
 {
     public interface IAttendanceProvider
     {
-        /// <summary>
-        /// Gets normalized attendance data from biometric source
-        /// </summary>
         Task<List<AttendanceDto>> GetAttendanceAsync(DateTime fromDate);
-
-        /// <summary>
-        /// Optional: future use for last punch aggregation
-        /// </summary>
+        Task<List<BiometricPunch>> GetPunchesAsync(DateTime fromDate, DateTime toDate);
         Task<List<EmployeeMapping>> GetLastPunchDataAsync();
-
-        /// <summary>
-        /// Optional: new employees detected from biometric system
-        /// </summary>
         Task<List<EmployeeMapping>> GetNewEmployeesAsync();
     }
 }
