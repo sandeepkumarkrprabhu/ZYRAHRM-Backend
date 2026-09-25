35fa6aaceb117342cbdd167291075adebc6dfb46
Sandeep <sandeepkumarkrprabhu@gmail.com>
2026-09-25 13:33:03 +0530

    Refactor attendance employee data behind dedicated service

---
+++ Zyra.LantimeServiceApp/Interfaces/IEmployeeAttendanceService.cs
@@ -0,0 +1,13 @@
+using ZyraHangfireModels.Models;
+
+namespace Zyra.LantimeServiceApp.Interfaces
+{
+    public interface IEmployeeAttendanceService
+    {
+        Task<Dictionary<string, EmployeeMapperDto>> GetEmployeeMappingsAsync(
+            IEnumerable<string> biometricUserIds);
+
+        Task<Dictionary<int, EmployeeAttendancePolicy>> GetEmployeeAttendancePoliciesAsync(
+            IEnumerable<int> employeeIds);
+    }
+}
